62ca4d5a5ea1bb125e391c8e014086005693b256
JulianMar <julianmarti96n@gmail.com>
2022-04-21 15:04:15 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-gzxdjh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-gzxdjh)
---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="https://vuejs.org/images/logo.png" />
+    {{ value }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Ref, ref, defineComponent } from 'vue';
+type test = {
+  booleanRef: Ref<Boolean>;
+  booleanNoRef: Boolean;
+};
+
+export default defineComponent({
+  setup() {
+    const value: Ref<test> = ref({
+      booleanRef: ref(true),
+      booleanNoRef: true,
+    });
+
+    value.value.booleanRef = ref(false);
+
+    return {
+      value,
+    };
+  },
+});
+</script>
